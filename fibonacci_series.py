54753b656b4953f77189726abf53baa991b6f8ef
Dhivyadarsshni <85487484+Dhivyadarsshni@users.noreply.github.com>
2021-06-15 14:38:13 +0530

    Create fibonacci_series.py

---
+++ fibonacci_series.py
@@ -0,0 +1,15 @@
+num=int(input("Enter a positive number: "))
+# enter a positive number
+
+n1=0
+n2=1
+print(n1)
+print(n2)
+i=2
+while i<=num:
+    n3=n1+n2
+    print(n3)
+    n1=n2
+    n2=n3
+    i+=1
+  
